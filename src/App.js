0aeb30bc5107f4cdde7e6b78f3eee427bfe7560c
Mike Mueller <muellerm.dev@gmail.com>
2020-01-11 15:48:59 -0500

    commit for transfer workstation

--- src/App.js
+++ src/App.js
@@ -18,31 +18,34 @@ class App extends Component {
 		}
 	}
 
+	// DATA IS ONLY BEING RECIEVED **AFTER** COMING FROM HOME PAGE AND STILL TAKES HALF A SECOND TO RENDER ONTO THE SCREEN
+	// ITS AN ISSUE AND I JUST WANT TO DIE
+	// A NOVELLUM BY ME, MR. HATE
+
 	componentDidMount() {
 		axios.get(`./data.json`)
 			.then(res => {
 				const restData = res.data.restaurants;
-				this.setState({ restData });
+				this.setState({ restData: restData });
 			});
 	}
 
 	updateCurrentRestaurant = (id) => {
 		this.setState({ currentRest: id });
-		console.log(id);
 	}
 
 	handleMenuState = () => {
 		this.setState({ menuView: !this.state.menuView });
 	}
 	render() {
-		console.log(this.state.restData[this.state.currentRest] ? this.state.restData[this.state.currentRest].name : 'nothing');
+		// console.log(this.state.restData[this.state.currentRest] ? this.state.restData[this.state.currentRest].name : 'nothing');
 		return (
 			<div className='App' >
 				<Header handleMenuState={this.handleMenuState} menuState={this.state.menuView} />
 				<Menu menuState={this.state.menuView} />
 				<Switch>
 					<Route exact path='/' render={(props) => <Home {...props} restData={this.state.restData} updateCurrentRestaurant={this.updateCurrentRestaurant} />} />
-					<Route exact path='/restaurant' Render={(props) => <Restaurant {...props} current={this.state.currentRest} restData={this.state.restData[this.state.currentRest]} />} />
+					<Route exact path='/restaurant' render={(props) => <Restaurant {...props} current={this.state.currentRest} restData={this.state.restData[this.state.currentRest]} />} />
 				</Switch>
 			</div>
 		);
--- src/pages/restaurant/Restaurant.component.jsx
+++ src/pages/restaurant/Restaurant.component.jsx
@@ -5,25 +5,56 @@ class Restaurant extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			title: 'placeholder',
+			title: '',
 			about: '',
 			style: '',
-			menu: []
+			menu: [],
+			menuItems: []
 		};
 
 	}
 
-	componentDidMount() {
+	updateStateWithProps = () => {
 		this.setState({ title: this.props.restData.name, about: this.props.restData.about, style: this.props.restData.style, menu: this.props.restData.menu });
+		console.log(this.state.menu);
+	}
+
+	componentDidMount() {
+		try {
+			this.updateStateWithProps();
+			if (this.props.restData) {
+			} else {
+
+				setTimeout(() => {
+					this.updateStateWithProps();
+					// this.createMenuItems();
+				}, 250);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	createMenuItems = () => {
+		let menu = this.state.menu.map(item => {
+			return (
+				<div className="MenuItem">
+					<h3>{item.name} - {item.price}</h3>
+					<p>{item.about}</p>
+				</div>
+			);
+		});
+		this.setState({ menuItems: menu });
+		console.log(this.state.menuItems);
 	}
 
 	render() {
-		// console.log(this.state.title);
-		// console.log(this.props.restData);
-		// Restaurant component will be template that accepts props to populate it's content for one of three different restaurants
 		return (
 			<div className='Restaurant'>
-				<h2>{this.props.restData.name}</h2>
+				<h2>{this.state.title}</h2>
+				<div className="Restaurant--menuList">
+					{this.state.menuItems}
+				</div>
 			</div>
 		);
 	}
