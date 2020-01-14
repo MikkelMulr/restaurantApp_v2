8bd03c4c7de3cb6f99cc4681c72236509542ab4a
Mike.Mueller <michael17mueller@gmail.com>
2020-01-14 00:46:44 -0500

    mhm

--- src/components/menu/Menu.component.jsx
+++ src/components/menu/Menu.component.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './Menu.style.scss';
 
 function Menu({ menuState }) {
-  return (
-    <div className={`Menu ${menuState ? 'unhidden' : 'hidden'}`}>
-      <h2>Menu</h2>
-    </div>
-  )
+	return (
+		<div className={`Menu ${menuState ? 'unhidden' : 'hidden'}`}>
+			<h2>Menu</h2>
+			<Link to='' />
+		</div>
+	);
 }
 
 export default Menu;
--- src/pages/restaurant/Restaurant.component.jsx
+++ src/pages/restaurant/Restaurant.component.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './Restaurant.style.scss';
 
 class Restaurant extends Component {
@@ -9,52 +10,67 @@ class Restaurant extends Component {
 			about: '',
 			style: '',
 			menu: [],
-			menuItems: []
+			menuItems: [],
+			restData: '',
+			error: { msg: '', occured: false }
 		};
-
 	}
 
-	updateStateWithProps = () => {
-		this.setState({ title: this.props.restData.name, about: this.props.restData.about, style: this.props.restData.style, menu: this.props.restData.menu });
-		console.log(this.state.menu);
-	}
-
-	componentDidMount() {
+	updateStateWithData = () => {
 		try {
-			this.updateStateWithProps();
-			if (this.props.restData) {
-			} else {
-
-				setTimeout(() => {
-					this.updateStateWithProps();
-					// this.createMenuItems();
-				}, 250);
-			}
+			this.setState({
+				title: this.state.restData[this.props.current].name,
+				about: this.state.restData[this.props.current].about,
+				style: this.state.restData[this.props.current].style,
+				menu: this.state.restData[this.props.current].menu,
+				restData: ''
+			});
 		} catch (error) {
 			console.log(error);
+			this.setState({ error: 'Uh something went wrong!', occured: true });
 		}
+	};
+
+	componentDidMount() {
+		axios
+			.get(`./data.json`)
+			.then((res) => {
+				const restData = res.data.restaurants;
+				this.setState({ restData: restData });
+			})
+			.then(() => {
+				this.updateStateWithData();
+			})
+			.then(() => {
+				this.createMenuItems();
+			});
 	}
 
 	createMenuItems = () => {
-		let menu = this.state.menu.map(item => {
+		let menu = this.state.menu.map((item) => {
 			return (
-				<div className="MenuItem">
-					<h3>{item.name} - {item.price}</h3>
-					<p>{item.about}</p>
+				<div className='MenuItem'>
+					<h3 className='MenuItem--title'>
+						{item.name} - <span className='MenuItem--price'>{item.price}</span>
+					</h3>
+					<p className='MenuItem--desc'>{item.about}</p>
 				</div>
 			);
 		});
 		this.setState({ menuItems: menu });
 		console.log(this.state.menuItems);
-	}
+	};
 
 	render() {
+		setTimeout(() => {
+			console.log(this.state.restData[this.props.current]);
+		}, 500);
+
 		return (
 			<div className='Restaurant'>
+				{this.state.error.occued ? <h2>{this.state.error.msg}</h2> : null}
 				<h2>{this.state.title}</h2>
-				<div className="Restaurant--menuList">
-					{this.state.menuItems}
-				</div>
+				<div className='Restaurant--menuList'>{this.state.menuItems}</div>
 			</div>
 		);
 	}
